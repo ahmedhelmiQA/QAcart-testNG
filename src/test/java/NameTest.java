d4a8556d472db6debc7f825ac37f88adfb2e4092
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-01-01 11:47:53 +0200

    BeforeClass-AfterClass + BeforeMethod-AfterMethod

--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -1,8 +1,26 @@
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class NameTest {
 
+    @BeforeClass  // before
+    public void beforeclass(){
+        System.out.println("i am inside the before class");
+    }
+    @AfterClass   // after
+    public void afterclass(){
+        System.out.println("i am inside the after class");
+    }
+
+
+@BeforeMethod   // before each
+public void beforemethod(){
+    System.out.println("i am inside the before method");
+}
+@AfterMethod   // after each
+public void aftermethod(){
+    System.out.println("i am inside the after method");
+}
 
 
     @Test
