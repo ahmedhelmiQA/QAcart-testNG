566c91581bd01c9a864afdc187aec22b44651922
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-01-01 13:44:36 +0200

    exclude or include testcase

--- src/test/java/NameTest2.java
+++ src/test/java/NameTest2.java
@@ -3,7 +3,20 @@ import org.testng.annotations.Test;
 
 public class NameTest2 extends Methods{
     @Test
-    public void should_returns_USER_if_the_name_contains_special_character1() {
+    public void C() {
+        System.out.println("Start With testcase 3");
+        String expextedResult = "USER";
+        String actualResult = Name.checkName("ah@md");
+        Assert.assertEquals(actualResult , expextedResult);
+    }
+    public void D() {
+        System.out.println("Start With testcase 3");
+        String expextedResult = "USER";
+        String actualResult = Name.checkName("ah@md");
+        Assert.assertEquals(actualResult , expextedResult);
+    }
+
+    public void E() {
         System.out.println("Start With testcase 3");
         String expextedResult = "USER";
         String actualResult = Name.checkName("ah@md");
