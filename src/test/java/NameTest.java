464a15a5ebbdc8b212d4579d0e1cad2abdf3dc61
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-01-01 13:02:28 +0200

    BeforeTest-AfterTest/BeforeSuite-AfterSuite

--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -1,19 +1,32 @@
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class NameTest extends Methods{
+public class NameTest{
 
+
+//    @Test
+//    public void should_returns_USER_if_the_name_contains_special_character2() {
+//        System.out.println("Start With testcase 1");
+//        String expextedResult = "USER";
+//        String  actualResult = Name.checkName("ahmed.");
+//        Assert.assertEquals(actualResult , expextedResult);
+//    }
+//    @Test
+//    public void should_returns_USER_the_correct_name() {
+//        System.out.println("Start With testcase 2");
+//        String expectedResult = "ahmed";
+//        String actualResult = Name.checkName("ahmed");
+//        Assert.assertEquals(actualResult , expectedResult);
+//    }
 
     @Test
-    public void should_returns_USER_if_the_name_contains_special_character2() {
-        System.out.println("Start With testcase 1");
+    public void A() {
         String expextedResult = "USER";
         String  actualResult = Name.checkName("ahmed.");
         Assert.assertEquals(actualResult , expextedResult);
     }
     @Test
-    public void should_returns_USER_the_correct_name() {
-        System.out.println("Start With testcase 2");
+    public void B() {
         String expectedResult = "ahmed";
         String actualResult = Name.checkName("ahmed");
         Assert.assertEquals(actualResult , expectedResult);
